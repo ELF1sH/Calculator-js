cf6a6cee9543e8fe43add7674ae67e47ee124be5
Igor Lezhnin <86882588+Ray-Janson@users.noreply.github.com>
2021-07-07 23:12:31 +0700

    logic foundation

--- js/RPN.js
+++ js/RPN.js
@@ -1,35 +1,111 @@
 // Reverse Polish Notation
 export class RPN {
     launch(inputValue) {
-        const s = new Stack()
+        let isExpressionBad = false
+
+        let elemArray = new Array()
         let currentNumber = ""
         for (const symbol of inputValue) {
-            if (symbol !== "+" && symbol !== "-" && symbol !== "*" && symbol !== "/"
+            if (symbol !== "+" && symbol !== "-" && symbol !== "×" && symbol !== "÷"
             && symbol !== "(" && symbol !== ")") {
                 currentNumber += symbol
             }
             else {
                 if (currentNumber !== "") {
-                    s.push(+currentNumber)
+                    elemArray.push(+currentNumber)
+                    currentNumber = ""
                 }
                 switch (symbol) {
                     case "+":
-                        s.push(new Sign(0))
+                        elemArray.push(new Sign(0))
                         break
                     case "-":
-                        s.push(new Sign(1))
+                        elemArray.push(new Sign(1))
                         break
-                    case "*":
-                        s.push(new Sign(2))
+                    case "×":
+                        elemArray.push(new Sign(2))
                         break
-                    case "/":
-                        s.push(new Sign(3))
+                    case "÷":
+                        elemArray.push(new Sign(3))
+                        break
+                    case "(":
+                        elemArray.push(new Sign(4))
+                        break
+                    case ")":
+                        elemArray.push(new Sign(5))
                         break
                 }
             }
         }
-        s.print()
-        console.log("-------------------------")
+        if (currentNumber !== "") elemArray.push(+currentNumber)
+        // elemArray is just array of elements in expression
+        // now we need to get RPN from this expression
+
+
+        // main algorithm
+        const s = new Stack()
+        let RPN = new Array()
+        for (const elem of elemArray) {
+            if (typeof elem === "number") {
+                RPN.push(elem)
+            }
+            else {
+                if (s.isEmpty()) {
+                    s.push(elem)
+                }
+                else {
+                    if (elem.status === 4) {
+                        s.push(elem)
+                    }
+                    else if (elem.status === 5) {
+                        while (s.getLast().status !== 4) {
+                            RPN.push(s.getLast())
+                            s.pop()
+                            if (s.isEmpty()) {
+                                isExpressionBad = true
+                                console.log("bad")
+                                return
+                            }
+                        }
+                        s.pop() // removing opening bracket
+                    }
+                    else {
+                        let currentSignPriority
+                        if (elem.status === 0 || elem.status === 1) currentSignPriority = 0
+                        else currentSignPriority = 1
+
+                        let lastSignPriority 
+                        let lastSign = s.getLast()
+                        if (lastSign.status === 0 || lastSign.status === 1) lastSignPriority = 0
+                        else lastSignPriority = 1
+
+                        while (currentSignPriority <= lastSignPriority) {
+                            RPN.push(lastSign)
+                            s.pop()
+                            
+                            if (s.isEmpty()) break
+                            lastSign = s.getLast()
+                            if (lastSign.status === 0 || lastSign.status === 1) lastSignPriority = 0
+                            else lastSignPriority = 1
+                        }
+
+                        s.push(elem)
+                    }
+                }
+            }
+        }
+        // end of the cycle
+
+        // adding the rest of the signs 
+        while (!s.isEmpty()) {
+            RPN.push(s.getLast())
+            s.pop()
+        }
+
+        for (const elem of RPN) {
+            console.log(elem)
+        }
+        console.log("------------------")
     }
 }
 
@@ -43,6 +119,17 @@ class Stack {
             console.log(elem)
         }
     }
+    isEmpty() {
+        if (this.arr.length === 0) return true
+        else return false
+    }
+    getLast() {
+        if (this.arr.length !== 0) return this.arr[this.arr.length - 1]
+        return null
+    }
+    pop() {
+        this.arr.pop()
+    }
 }
 
 class Sign {
@@ -52,5 +139,7 @@ class Sign {
         // 1 - minus
         // 2 - multiplication
         // 3 - division
+        // 4 - opening bracket
+        // 5 - closing bracket
     }
 }

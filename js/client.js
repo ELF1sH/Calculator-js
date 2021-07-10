fecacd587bc56b3956fb3e40f662c62418490d9b
Igor Lezhnin <86882588+Ray-Janson@users.noreply.github.com>
2021-07-10 23:09:22 +0700

    C btn behavior

--- js/client.js
+++ js/client.js
@@ -2,23 +2,58 @@ import { RPN } from "./RPN.js"
 const RPNalgo = new RPN()
 const calcInput = document.getElementById("calc-input")
 const btns = document.getElementsByClassName("btn-calc")
+const Cbtn = document.getElementById("C-btn")
 
 export function setBtnListeners() {
     for (const btn of btns) {
         btn.addEventListener("click", (event) => {
             const symbol = event.currentTarget.children[0].innerHTML
             if (symbol !== 'C' && symbol !== '=') {
-                calcInput.value += symbol
-                calcInput.focus()  // otherwise cursor doesn't follow symbols from btns
-                calcLogic()
+                calcInput.value += symbol        
             }
-            else if (symbol == 'C') {
-                calcInput.value = ""
-            }
+            calcInput.focus()  // otherwise cursor doesn't follow symbols entering from btns
+            calcLogic()
+            inputSizeHandler()
         })
     }
+
+    Cbtn.addEventListener("click", deleteLastSymbol)
+
+    
+    // delete all input by holding C button
+    let isHolding = true
+    function clearInput() {
+        if (isHolding) calcInput.value = ""
+    }
+
+    Cbtn.addEventListener("mousedown", (event) => {
+        const symbol = event.currentTarget.children[0].innerHTML
+        if (symbol === 'C') {
+            isHolding = true
+            setTimeout(clearInput, 700)
+        }
+    })
+    Cbtn.addEventListener("mouseup", () => {
+        isHolding = false
+    })
+}
+
+function deleteLastSymbol() {
+    calcInput.value = calcInput.value.substring(0, calcInput.value.length - 1)
 }
 
 function calcLogic(RPN) {   
     RPNalgo.launch(calcInput.value)
+}
+
+function inputSizeHandler() {
+    if (calcInput.value.length > 16) {
+        calcInput.style.fontSize = "1.5rem"
+    }
+    else if (calcInput.value.length > 10) {
+        calcInput.style.fontSize = "2rem"
+    }
+    else {
+        calcInput.style.fontSize = "3rem"
+    }
 }
